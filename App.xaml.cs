f4fe37f1a477268fa3fe6bf3225147ba16bbc033
Tatsunoshin <wccrs287@yahoo.co.jp>
2016-12-18 02:51:20 +0900

    初期状態

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace ForMyFather
+﻿namespace ForMyFather
 {
-	/// <summary>
-	/// App.xaml の相互作用ロジック
-	/// </summary>
+	using System.Windows;
+	using ForMyFather.View;
+
+	/// <summary> 
+	    /// App.xaml の相互作用ロジック 
+	    /// </summary> 
 	public partial class App : Application
 	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			var v = new MainWindow();
+			v.Show();
+		}
 	}
 }
